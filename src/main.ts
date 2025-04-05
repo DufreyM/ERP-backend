4fcadb28e7064aac16d2850da010a5f5573e1445
Leonardo Mejía <leomejia646@gmail.com>
2025-04-05 14:54:50 -0600

    Permitir cors para las peticiones

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // 🚨 Aquí habilitamos CORS para permitir peticiones desde el frontend
+  app.enableCors({
+    origin: 'http://localhost:3001', // Dirección de tu frontend
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
